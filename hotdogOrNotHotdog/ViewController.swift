877fa0227239674c62a15037fbc257a67bd2f30a
Karan Gandhi <kag354@rutgers.edu>
2021-11-29 18:03:32 -0800

    class exittt

--- hotdogOrNotHotdog/ViewController.swift
+++ hotdogOrNotHotdog/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+
+    
+    
+    
+    
+    // Delegation design pattern -> Text field: Properties
+    
+    // Steps to implement a delegate:
+    // 1. Create an object
+    
+    let imagePicker = UIImagePickerController()
+    let results : [VNClassificationObservation] = []
+    
+//Type Annotation    //[0.8, 0.4, 0.3] -> 0.8 -> Hotdog
+    
+    
+
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //2. Initialize the delegate in View Did Load
+        imagePicker.delegate = self
+        
+        
+        
+        
     }
+    
 
+    
 
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .camera
+        
+        
+        
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    func detect(){
+        
+        
+    }
+    
 }
 
