5d442dd101cea2c309b7cc6a4594e0481c1911a2
Karan Gandhi <kag354@rutgers.edu>
2021-12-01 14:45:30 -0800

    exit class

--- hotdogOrNotHotdog/ViewController.swift
+++ hotdogOrNotHotdog/ViewController.swift
@@ -1,75 +1,99 @@
 //
 //  ViewController.swift
-//  hotdogOrNotHotdog
+//  imageRecognitionApp
 //
-//  Created by Karan Gandhi on 11/29/21.
+
 //
 
 import UIKit
 import CoreML
 import Vision
 
-
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
-
-    
-    
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // Delegation design pattern -> Text field: Properties
     
     // Steps to implement a delegate:
     // 1. Create an object
-    
     let imagePicker = UIImagePickerController()
-    let results : [VNClassificationObservation] = []
     
-//Type Annotation    //[0.8, 0.4, 0.3] -> 0.8 -> Hotdog
-    
-    
-
-    
+    var result: [VNClassificationObservation] = []
     
     @IBOutlet weak var imageView: UIImageView!
     
-    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //2. Initialize the delegate in View Did Load
-        imagePicker.delegate = self
         
+        imagePicker.delegate = self    // 2. Initialize the delegate
         
-        
-        
+        // Do any additional setup after loading the view.
     }
-    
-
-    
-
-    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
-        
-        imagePicker.sourceType = .camera
-        
-        
-        
-        
-    }
-    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        
-        
-        
+        // 1. Select the image
+        // 2. Convert this image from UIImage data type into CIImage
+        // 3. Detect (CIImage)
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+            
+            // Convert to CIImage
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("Failed to convert UIImage to CIImage.")
+            }
+            
+            detect(image: ciImage)
+        }
     }
     
+    // Detect function:
+    // 1. Model
+    // 2. Request
+    // 3. Result
+    // 4. Handler
     
-    func detect(){
-        
+    func detect(image: CIImage) {
+        // Initialize Model
+        if let model = try? VNCoreMLModel(for: Inceptionv3().model) {
+            // Request
+            let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                // Results -> (0.8, 0.7, 0.3) -> 0.8: Hot Dog
+                guard let results = request.results as? [VNClassificationObservation], let topResult = results.first else {
+                    fatalError("Could not get the correct data classification value.")
+                }
+                
+                if topResult.identifier.contains("hotdog") {
+                    // Using main thread
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Hotdog"
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not Hotdog!"
+                    }
+                }
+            })
+            
+            // Handler
+            let handler = VNImageRequestHandler(ciImage: image)
+            do {
+                try handler.perform([request])
+            }
+            catch {
+                print(error)
+            }
+            
+        }
         
     }
-    
-}
 
+}
